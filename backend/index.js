50608fcad4487b99e06a6a1db2b71a18af903f53
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-12-06 05:32:45 +0530

    api route is created and use middleware that is express.json()

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const connect_to_mongo = require('./db');
 const express = require('express')
 
+
 connect_to_mongo();
 const app = express()
 const port = 5000
 
+app.use(express.json());
+app.use('/api/auth' , require('./routes/auth'));
 
 app.get('/', (req, res) => {
     res.send('Hello Ritesh!')
